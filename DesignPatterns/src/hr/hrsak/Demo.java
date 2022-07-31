eb782c82e808eadc99f94e0f6f3d44a041258fc2
fhrsak <hrsak.filip@gmail.com>
2022-07-31 22:02:13 +0200

    Decorator

--- DesignPatterns/src/hr/hrsak/Demo.java
+++ DesignPatterns/src/hr/hrsak/Demo.java
@@ -3,6 +3,7 @@ package hr.hrsak;
 public class Demo {
     public static void main(String[] args) {
 
-
+        final MagicString s = new MagicString("Hello");
+        System.out.println(s + " has " + s.getNumberOfVowels() + " number of vowels");
     }
 }
